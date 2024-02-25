24e99c0f638b223776d9a51a6d0e0834860eaac3
sw3476e <s.wolf@fh-aachen.de>
2024-02-25 21:42:37 +0100

    Fix typo

--- Implementation/Hardware/MoistureSensor.cpp
+++ Implementation/Hardware/MoistureSensor.cpp
@@ -7,11 +7,11 @@
 
 // Calibration
 #define MOISTURE_ZERO 3660.0
-#define MOISTURE_HUNDRET 3881.0
+#define MOISTURE_HUNDRED 3881.0
 
 
 double MoistureSensor::getValue() {
     uint16_t raw = sensorpin.adcReadRaw(); // Value between 0..4095
-    double value = ((4095-raw) - MOISTURE_ZERO) / (MOISTURE_HUNDRET - MOISTURE_ZERO);
+    double value = ((4095-raw) - MOISTURE_ZERO) / (MOISTURE_HUNDRED - MOISTURE_ZERO);
     return value * 100.0; // Output in %
 }
